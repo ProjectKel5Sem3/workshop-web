595b593a5fb49829cb76407f8b57a632d7abd099
Dimas <124556325+Diz09@users.noreply.github.com>
2023-11-30 14:36:02 +0700

    script login to api

--- website-shila/login-admin/login.php
+++ website-shila/login-admin/login.php
@@ -38,11 +38,40 @@
 </div>
 
 <script>
-    // function togglePassword() {
-    //     var passwordInput = document.getElementById('password');
-    //     passwordInput.type = (passwordInput.type === 'password') ? 'text' : 'password';
-    // }
+    document.addEventListener('DOMContentLoaded', function() {
+        var loginForm = document.getElementById('loginForm');
+
+        loginForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            var userEmail = document.getElementById('your-email').value;
+            var userPassword = document.getElementById('password').value;
+            //ip = cmd -> ipconfig -> IPv4 Address -> 000.000.0.0
+            fetch('http://localhost/a/github/workshop-web/dari%20dimas/API/api_users.php?action=login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded', },
+                body: 'user_email=' + encodeURIComponent(userEmail) + '&user_password=' + encodeURIComponent(userPassword),
+            })
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                if (data.code === 200) {
+                    window.location.href = '../admin/dashboard/dashboard.html';
+                } else if (data.code === 401) {
+                    alert('salah')
+                } else {
+                    alert('login Gagal. Username atau password salah.');
+                }                
+            })
+            .catch(function(error) {
+                console.error('Error:, error');
+                alert('terjadi kesalahan')
+            })
+        })
+    })
 </script>
+
 
 </body>
 </html>
